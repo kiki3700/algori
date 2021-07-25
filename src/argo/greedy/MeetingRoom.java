abec0e346cc6229aa8ff0029e71670af0dd36036
kiki3700 <5575492q@gmail.com>
2021-07-25 21:56:10 +0900

    greedy 'activity selection problem

---
+++ src/argo/greedy/MeetingRoom.java
@@ -0,0 +1,40 @@
+package argo.greedy;
+
+import java.util.*;
+
+public class MeetingRoom {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count = 0;
+		int end = 0;
+		int[][] arr = new int[n][2];
+		for(int i=0; i<n ;i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+	}
+		
+		Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				if(o1[1]==o2[1]) {
+					return o1[0]-o2[0];
+				}
+				return  o1[1]-o2[1];
+			}
+			
+		});
+
+		for(int i = 0 ; i < n ; i++) {
+			if(end <= arr[i][0]) {
+				end = arr[i][1];
+				count++;
+			}
+		}
+		
+		
+		System.out.println(count);
+	}	
+}
