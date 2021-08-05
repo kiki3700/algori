4f821f02b8d951b32ddd3467134ecadb258bb180
kiki3700 <5575492q@gmail.com>
2021-08-05 23:09:21 +0900

    composition

--- src/argo/bruteForce/Decomposition.java
+++ src/argo/bruteForce/Decomposition.java
@@ -3,18 +3,25 @@ package argo.bruteForce;
 import java.util.*;
 
 public class Decomposition {
-	static int n;
-	static int len;
-	static PriorityQueue<Integer> pq;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		pq = new PriorityQueue();
-		len = (n+"").length();
-		for(int i =0; i<len; i++) {
-			for(int j =0; j<10j++) {
-				(Math.pow(10, i)+1)*j;
-			}
-		}
-	}
+    static int n;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int ans=0;
+        for(int i = 0; i<n;i++){
+            if(deco(i)+i==n){
+                ans = i;
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+    static int deco(int num){
+        int sum =0;
+        while(num>0){
+            sum += num%10;
+            num /=10;
+        }
+        return sum;
+    }
 }
