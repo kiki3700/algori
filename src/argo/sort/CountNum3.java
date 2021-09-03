4a868e2bc36841876f5ce44ff85786a226e70480
kiki3700 <5575492q@gmail.com>
2021-09-03 20:01:14 +0900

    count num3

---
+++ src/argo/sort/CountNum3.java
@@ -0,0 +1,21 @@
+package argo.sort;
+import java.io.*;
+import java.util.*;
+public class CountNum3 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int[] arr = new int[10000];
+		BufferedReader bf = new BufferedReader(new InputStreamReader (System.in));
+		BufferedWriter bw = new BufferedWriter ( new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(bf.readLine());
+		for(int i = 0; i<num; i++) {
+			int a= Integer.parseInt(bf.readLine());
+			arr[a-1]++;
+		}
+		for(int i =0; i<10000; i++) {
+			for(int j = 0; j < arr[i];j++) {
+				bw.write(i+1+"\n");
+			}
+		}
+		bw.flush();
+	}
+}
