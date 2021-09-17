7fe1416ba8e1d5e9a10363d10cbbd3ffceeb7dc0
kiki3700 <5575492q@gmail.com>
2021-09-17 20:09:10 +0900

    seriesSort2

--- src/argo/sort/SeriesSort2.java
+++ src/argo/sort/SeriesSort2.java
@@ -5,40 +5,31 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.*;
+
 
 public class SeriesSort2 {
 	static int n;
 	static Num[] numArr;
-	static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine());
-		String str1[] = br.readLine().split(" ");
-		Num[] arr = new Num[n];
-		for(int i = 0; i< n; i++) {
-			arr[i] = new Num(Integer.parseInt(str1[i]),i);
+		n = Integer.parseInt(br.readLine());
+		numArr = new Num[n];
+		String[] tokens= br.readLine().split(" ");
+		for(int i = 0; i < n; i++) {
+			int num = Integer.parseInt(tokens[i]);
+			numArr[i] = new Num(num, i);
 		}
-		int[] ind = new int[n];
-		for(int i = 0 ; i <n; i++) {
-			int min = arr[0].val;
-			int minInd = 0;
-			for(int j = 1; j< n; j++) {
-				if(arr[minInd].val>arr[j].val) {
-					if(arr[min].val>arr[j].val) {
-						min = arr[j].val;
-						minInd = j;
-					}
-				}
-				ind[i]= minInd;
-				arr[minInd].val = 212121;
-			}
-			
+		seriesSorts(numArr);
+		for(int i = 0 ; i < n; i++) {
+			int val = numArr[i].ind;
+			numArr[i].val = val;
+			numArr[i].ind = i;
 		}
-		for(int i =0; i<n; i++) {
-			for(int j = 0; j<n;j++) {
-				if(arr[i].ind==ind[j]) bw.write(j+" ");
-			}
+		seriesSorts(numArr);
+		for(int i = 0 ; i < n ; i++) {
+			bw.write(numArr[i].ind+" ");
 		}
 		bw.flush();
 	}
@@ -49,5 +40,23 @@ public class SeriesSort2 {
 			this.val = v;
 			this.ind = i;
 		}
+	}
+	static void seriesSorts(Num[] numArr) {
+		Arrays.sort(numArr, new Comparator<Num>() {
+			@Override
+			public int compare(Num o1, Num o2) {
+				if(o1.val<o2.val) {
+					return -1;
+				}else if(o1.val>o2.val) {
+					return 1;
+				}else {
+					if(o1.ind>o2.ind) {
+						return 1;
+					}else {
+						return -1;
+					}
+				}
+			}
+		});
 	}
 }
