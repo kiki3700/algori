c9ef096b8214bc48662af0d3c78eb66bb7f84016
kiki3700 <5575492q@gmail.com>
2021-10-31 10:55:03 +0900

    hongik

---
+++ src/argo/HonikUniv.java
@@ -0,0 +1,10 @@
+package argo;
+import java.util.*;
+public class HonikUniv {
+		public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int base = 1946;
+        System.out.print(n - base);
+    }
+}
