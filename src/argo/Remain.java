41195456976e05e7679d9aadf4e5c998a5761955
kiki3700 <5575492q@gmail.com>
2021-11-21 10:27:31 +0900

    remain

---
+++ src/argo/Remain.java
@@ -0,0 +1,15 @@
+package argo;
+import java.util.*;
+public class Remain {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+         System.out.println(a*b%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
