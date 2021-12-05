30b061cbf34b779bfe3c46f245887febb8216be3
kiki3700 <5575492q@gmail.com>
2021-12-05 16:24:04 +0900

    sheep

--- src/argo/sort/Sheep.java
+++ src/argo/sort/Sheep.java
@@ -3,11 +3,11 @@ package argo.sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Sheep {
@@ -16,13 +16,13 @@ public class Sheep {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		LinkedList<Integer> cList= new LinkedList<>();
+		ArrayList<Integer> cList= new ArrayList<>();
 		for(int i = 0 ; i< n ; i++) {
 			cList.add(Integer.parseInt(st.nextToken()));
 		}
 		int k = Integer.parseInt(br.readLine());
 		Integer[] bArr = new Integer[k];
-		LinkedList<Integer> bList = new LinkedList<>();
+		ArrayList<Integer> bList = new ArrayList<>();
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0 ; i <k ;i++) {
 			bList.add(Integer.parseInt(st.nextToken())); 
@@ -33,21 +33,22 @@ public class Sheep {
 		int time = 0;
 		if(bList.get(0)>cList.get(0)) {
 			System.out.println(-1);
-			return;
-		}
-		while(!bList.isEmpty()){
-			int idx =0;
-			for(int i = 0; i < n;) {
-				if(idx == bList.size()) break;
-				else if(bList.get(idx)<=cList.get(i)) {
-					bList.remove(idx);
-					i++;
-				}else {
-					idx++;
+		
+		}else {
+			while(!bList.isEmpty()){
+				int idx =0;
+				for(int i = 0; i < n;) {
+					if(idx == bList.size()) break;
+					else if(bList.get(idx)<=cList.get(i)) {
+						bList.remove(idx);
+						i++;
+					}else {
+						idx++;
+					}
 				}
+				time++;
 			}
-			time++;
-		}
-		System.out.println(time);	
+			System.out.println(time);
+	}
 	}
 }
