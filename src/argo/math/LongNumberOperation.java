f43d32ef521c49c77d9e1756417d0abe669d039c
kiki3700 <5575492q@gmail.com>
2021-12-13 23:57:22 +0900

    long number operation

---
+++ src/argo/math/LongNumberOperation.java
@@ -0,0 +1,18 @@
+package argo.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class LongNumberOperation {
+	static BufferedReader br;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		BigInteger a = new BigInteger(br.readLine());
+		BigInteger b =  new BigInteger(br.readLine());
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+	}
+}
