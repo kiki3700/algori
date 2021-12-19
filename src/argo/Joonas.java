1fd1f15cc3835fce2ac371ea197446ae9ca13b7f
kiki3700 <5575492q@gmail.com>
2021-12-19 09:25:49 +0900

    joonas

---
+++ src/argo/Joonas.java
@@ -0,0 +1,13 @@
+package argo;
+
+import java.util.Scanner;
+
+public class Joonas {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		sb.append(sc.next());
+		sb.append("??!");
+		System.out.println(sb.toString());
+	}
+}
