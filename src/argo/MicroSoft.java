a1088f9261d03410ebcbdab094cc66f50749886c
kiki3700 <5575492q@gmail.com>
2021-12-24 11:13:23 +0900

    micro soft

---
+++ src/argo/MicroSoft.java
@@ -0,0 +1,11 @@
+package argo;
+
+public class MicroSoft {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-;;-._|\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-''-._|");
+	}
+}
