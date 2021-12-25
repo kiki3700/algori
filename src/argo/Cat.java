c1e9d1a0efd86d40d4240ce162db4c23b8391c6a
kiki3700 <5575492q@gmail.com>
2021-12-25 23:49:31 +0900

    cat

---
+++ src/argo/Cat.java
@@ -0,0 +1,12 @@
+package argo;
+
+import java.util.Scanner;
+
+public class Cat {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|");
+	}
+}
