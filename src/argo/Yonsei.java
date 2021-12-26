269dcc0d260d144b6b09e6accc0edaf889a06950
kiki3700 <5575492q@gmail.com>
2021-12-26 23:52:21 +0900

    yonsei

---
+++ src/argo/Yonsei.java
@@ -0,0 +1,14 @@
+package argo;
+
+import java.util.Scanner;
+
+public class Yonsei {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		if(a==0) System.out.println("YONSEI");
+		else System.out.println("Leading the Way to the Future");
+				
+	}
+}
