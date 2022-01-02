0cdca2c65e5f854b2c171b6349c0f09ecc5586ff
kiki3700 <5575492q@gmail.com>
2022-01-03 01:17:10 +0900

    king queenetc

---
+++ src/argo/KingQuenetc.java
@@ -0,0 +1,20 @@
+package argo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class KingQuenetc {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int[] arr = {1,1,2,2,2,8};
+		for(int i = 0 ; i < arr.length;i++) {
+			int tmp = Integer.parseInt(st.nextToken());
+			sb.append(arr[i]-tmp+" ");
+		}
+		System.out.println(sb.toString());
+	}
+}
