748618db8fba86ea0d800a3c69eb91ea401c5720
kiki3700 <5575492q@gmail.com>
2022-01-05 10:26:46 +0900

    student num

---
+++ src/argo/implement/StudentNumber.java
@@ -0,0 +1,41 @@
+package argo.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StudentNumber {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		
+		int n =Integer.parseInt(br.readLine());
+		String num = new String();
+		String[] numArr = new String[n];
+		for(int i = 0 ; i< n ; i++) {
+			numArr[i] = br.readLine();
+			
+		}
+
+		int len =numArr[0].length();
+		for(int i=1; i <=len; i++) {
+			Set<String> set = new HashSet<>();
+			boolean flag = false;
+			for(int j = 0; j < n ; j++) {
+				String str = numArr[j].substring(len-i);
+				System.out.println(str);
+				if(set.contains(str)) {
+					flag = true;
+					break;
+				}else {
+					set.add(str);
+				}
+			}
+			if(!flag) {
+				System.out.println(i);
+				return;
+			}
+		}
+	}
+}
