8ccfbb3d396542c9bb41a8ccb3c6573915916458
kiki3700 <5575492q@gmail.com>
2022-01-18 16:22:51 +0900

    math is pa;

---
+++ src/argo/math/MathIsPA.java
@@ -0,0 +1,12 @@
+package argo.math;
+
+import java.util.Scanner;
+
+public class MathIsPA {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in
+				);
+		System.out.print(sc.nextLong()*4);
+		
+	}
+}
