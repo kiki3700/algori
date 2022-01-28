cb501b1b2ce6d84ff631cd270d7cca238accad3c
kiki3700 <5575492q@gmail.com>
2022-01-28 11:03:20 +0900

    operation

--- src/argo/Operation.java
+++ src/argo/Operation.java
@@ -1,5 +1,16 @@
 package argo;
 
-public class Operation {
+import java.util.Scanner;
 
+public class Operation {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
 }
