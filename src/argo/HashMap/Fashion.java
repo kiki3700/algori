412f18c6a4db8907ded030e0e6b46c95e1262c79
kiki3700 <5575492q@gmail.com>
2022-02-09 10:28:05 +0900

    fashion

---
+++ src/argo/HashMap/Fashion.java
@@ -0,0 +1,32 @@
+package argo.HashMap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Fashion {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < n ; i++) {
+			int m = Integer.parseInt(br.readLine());
+			HashMap<String, Integer> map = new HashMap<>();
+			for(int j = 0 ; j < m ; j++) {
+				StringTokenizer st  = new StringTokenizer(br.readLine());
+				String item = st.nextToken();
+				String category = st.nextToken();
+				map.put(category, map.getOrDefault(category, 0)+1);
+			}
+			int sum = 1;
+			for(String key : map.keySet()) {
+				sum *= map.get(key)+1;
+			}
+			sum-=1;
+			sb.append(sum+"\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
