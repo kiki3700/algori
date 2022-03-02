799b26401a48ea32af1b477e58c99f790b8dc92a
kiki3700 <5575492q@gmail.com>
2022-03-02 20:18:14 +0900

    enrollment

---
+++ src/argo/Enrollment.java
@@ -0,0 +1,8 @@
+package argo;
+
+public class Enrollment {
+	public static void main(String[] args) {
+		System.out.println("260");
+		System.out.println("kiki3700");
+	}
+}
---
+++ src/argo/Joshep.java
@@ -0,0 +1,5 @@
+package argo;
+
+public class Joshep {
+
+}
