c621641c4fae83408fad3cc601bd0de5a77e68e4
kiki3700 <5575492q@gmail.com>
2022-03-03 22:32:56 +0900

    fighting

--- src/argo/Joshep.java
+++ src/argo/Joshep.java
@@ -1,5 +1,14 @@
 package argo;
 
 public class Joshep {
-
+	public static int findMinJump(int[] arr, int target) {
+		if(arr==null || arr.length==0) {
+			return 0;
+		}
+		int tmp = arr[target];
+		int[] newArr = new int[arr.length-1];
+		
+		
+		
+	}
 }
