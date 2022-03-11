ccce45e38b07e2783338cdd22078f73ea1d24234
kiki3700 <5575492q@gmail.com>
2022-03-12 08:37:29 +0900

    calculator

---
+++ src/argo/Calculator.java
@@ -0,0 +1,28 @@
+package argo;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int result=a;
+  
+       outline: while(true) {
+          
+               String temp=sc.next();
+               if(temp.contentEquals("=")) {
+                   break outline;
+               }
+               int b=sc.nextInt();
+              
+               if(temp.contentEquals("/")){ result=result/b;} 
+               if(temp.contentEquals("+")){ result=result+b;} 
+               if(temp.contentEquals("-")){ result=result-b;} 
+               if(temp.contentEquals("*")){ result=result*b;} 
+                   
+               }
+           
+          System.out.println(result);
+               }
+           }
