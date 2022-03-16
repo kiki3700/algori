78e954418fe4f1fda5b3be3fceeb0226599c1d0e
kiki3700 <5575492q@gmail.com>
2022-03-16 22:08:34 +0900

    buddist year

---
+++ src/argo/BudistYear.java
@@ -0,0 +1,12 @@
+package argo;
+
+import java.util.Scanner;
+
+public class BudistYear {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int base = sc.nextInt();
+		
+		System.out.println(base-543);
+	}
+}
